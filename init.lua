94dcfcafc7f07da5ec8111f60f7ece8e4ae6e8b0
atyou123 <ayou@andrew.cmu.edu>
2024-05-31 18:26:07 -0500

    added git

--- init.lua
+++ init.lua
@@ -4,15 +4,15 @@ require("core.autocommands")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{import = "plugins.general"}, {import = "plugins.language"}, {import = "plugins.ui"}})
+require("lazy").setup({ { import = "plugins.general" }, { import = "plugins.language" }, { import = "plugins.ui" } })
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,58 +2,58 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local no_recursive_mappings = {
-	n = { -- normal
-		-- moving between multiple windows
-		["<C-h>"] = "<C-w>h",
-		["<C-j>"] = "<C-w>j",
-		["<C-k>"] = "<C-w>k",
-		["<C-l>"] = "<C-w>l",
-		-- resize window
-		["<leader><Up>"] = ":resize +3<CR>",
-		["<leader><Down>"] = ":resize -3<CR>",
-		["<leader><Left>"] = ":vertical resize -3<CR>",
-		["<leader><Right>"] = ":vertical resize +3<CR>",
-		-- go to different buffers
-		["L"] = ":bnext<CR>",
-		["H"] = ":bprevious<CR>",
-		["J"] = "<Nop>",
-		["K"] = "<Nop>",
-		-- delete no yank
-		["d"] = '"_d',
-		["<leader>d"] = "d",
-	},
-	v = { -- visual
-		-- stay in visual mode when tabbing
-		[">"] = ">gv",
-		["<"] = "<gv",
-		-- move a block
-		["J"] = ":move '>+1<CR>gv-gv",
-		["K"] = ":move '<-2<CR>gv-gv",
-		-- delete/paste no yank
-		["d"] = '"_d',
-		["<leader>d"] = "d",
-		["p"] = '"_dP',
-		["<leader>p"] = "P",
-		["P"] = '"_dP',
-		["<leader>P"] = "P",
-	},
-	i = { -- insert
+    n = { -- normal
+        -- moving between multiple windows
+        ["<C-h>"] = "<C-w>h",
+        ["<C-j>"] = "<C-w>j",
+        ["<C-k>"] = "<C-w>k",
+        ["<C-l>"] = "<C-w>l",
+        -- resize window
+        ["<leader><Up>"] = ":resize +3<CR>",
+        ["<leader><Down>"] = ":resize -3<CR>",
+        ["<leader><Left>"] = ":vertical resize -3<CR>",
+        ["<leader><Right>"] = ":vertical resize +3<CR>",
+        -- go to different buffers
+        ["L"] = ":bnext<CR>",
+        ["H"] = ":bprevious<CR>",
+        ["J"] = "<Nop>",
+        ["K"] = "<Nop>",
+        -- delete no yank
+        ["d"] = '"_d',
+        ["<leader>d"] = "d",
     },
-	x = { -- visual block
-		-- delete/paste no yank
-		["d"] = '"_d',
-		["<leader>d"] = "d",
-		["p"] = '"_dP',
-		["<leader>p"] = "P",
-		["P"] = '"_dP',
-		["<leader>P"] = "P",
-	},
+    v = { -- visual
+        -- stay in visual mode when tabbing
+        [">"] = ">gv",
+        ["<"] = "<gv",
+        -- move a block
+        ["J"] = ":move '>+1<CR>gv-gv",
+        ["K"] = ":move '<-2<CR>gv-gv",
+        -- delete/paste no yank
+        ["d"] = '"_d',
+        ["<leader>d"] = "d",
+        ["p"] = '"_dP',
+        ["<leader>p"] = "P",
+        ["P"] = '"_dP',
+        ["<leader>P"] = "P",
+    },
+    i = { -- insert
+    },
+    x = { -- visual block
+        -- delete/paste no yank
+        ["d"] = '"_d',
+        ["<leader>d"] = "d",
+        ["p"] = '"_dP',
+        ["<leader>p"] = "P",
+        ["P"] = '"_dP',
+        ["<leader>P"] = "P",
+    },
     t = {
         ["<Esc>"] = "<C-\\><C-n>",
-		["<C-h>"] = "<C-\\><C-N><C-w>h",
-		["<C-j>"] = "<C-\\><C-N><C-w>j",
-		["<C-k>"] = "<C-\\><C-N><C-w>k",
-		["<C-l>"] = "<C-\\><C-N><C-w>l",
+        ["<C-h>"] = "<C-\\><C-N><C-w>h",
+        ["<C-j>"] = "<C-\\><C-N><C-w>j",
+        ["<C-k>"] = "<C-\\><C-N><C-w>k",
+        ["<C-l>"] = "<C-\\><C-N><C-w>l",
     },
 }
 
